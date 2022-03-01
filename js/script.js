f46ab24e549f9b9e28a24db0566c9accc1bb00aa
John Doe <hamedhasan422421@gmail.com>
2022-03-01 11:30:16 +0300

    spinner added

--- js/script.js
+++ js/script.js
@@ -1,21 +1,61 @@
 // Start Js 
-
+const showMobile = document.getElementById('show-phone')
+const details = document.getElementById('show-detail')
+const loadButton = document.getElementById('loadMore')
+const showLengths = document.getElementById('show-length')
+const success = document.getElementById('success')
+const error = document.getElementById('error')
+// const load = document.getElementById('load-button')
 // Input Field Search 
+
+
 const searchPhone = () => {
     const inputField = document.getElementById('input-field')
     const searchText = inputField.value
-    inputField.value = '';
+    // inputField.value = ''
+if(!isNaN(searchText) || searchText == ''){
+    error.innerHTML = 'Please Search By Phone Name ✘'
+    showMobile.innerHTML = ''
+    loadButton.innerHTML = ''
+    success.innerHTML = ''
+    showLengths.innerHTML = ''
+    inputField.value = ''
+    
+}else if(searchText <= 0){
+    showMobile.innerHTML = ''
+    loadButton.innerHTML = ''
+    success.innerHTML = ''
+    showLengths.innerHTML = ''
+}else{
     // Get Api 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-
-    fetch(url)
+            fetch(url)
         .then(res => res.json())
         .then(data => displayPhone(data.data))
+        .catch(error => displayError(error))
+        error.innerHTML = ''
+        details.innerHTML = ''
+       success.innerHTML = `Great You Got The Phone 😎 ${searchText}`
+    //    inputField.value = ''
+    toggleSpinner('block')
+}
 }
 
-// display data 
+const displayError = () =>{
+    success.innerHTML = ''
+    document.getElementById('load-button').style.display = 'none'
+   }
 
+// spinner 
+toggleSpinner = displaySpinner =>{
+    document.getElementById('spinner').style.display = displaySpinner
+}
+
+
+// display data 
+const showLength = document.getElementById('show-length')
 const displayPhone = phones => {
+    showLength.innerHTML = `${phones.length}`
     const showPhone = document.getElementById('show-phone')
     showPhone.textContent = '';
     const allPhone = phones.slice(0, 20)
@@ -37,25 +77,34 @@ const displayPhone = phones => {
 
     })
 
+    toggleSpinner('none')
     // add load more button 
     const loadMore = document.getElementById('loadMore')
+    loadMore.textContent = ''
+    // load.innerHTML = ''
      const loadDiv = document.createElement('div')
      loadDiv.innerHTML = `
      <div class="text-center">
-     <button onclick="loadBtn('${phones}')" class="btn btn-outline-primary">Load More</button>
+     <button id="load-button" onclick="loadBtn()" class="btn btn-outline-primary">Load More</button>
      `
      loadMore.appendChild(loadDiv)
-    
 }
-const loadBtn = more => {
+const loadBtn = () => {
+    // console.log('hello')
+    const inputField = document.getElementById('input-field')
+    const searchText = inputField.value
+    // inputField.value = '';
     // more.slice(0,100)
-    const url = `https://openapi.programming-hero.com/api/phones?search`
+    // console.log(searchText);
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayMore(data.data))
+    toggleSpinner('block')
 }
+
 const displayMore = (mores) => {
-    const morePhone = document.getElementById('show-phone')
+const morePhone = document.getElementById('show-phone')
     const allMores = mores.slice(20, 179)
 allMores.forEach(more => {
     const div = document.createElement('div')
@@ -66,13 +115,14 @@ allMores.forEach(more => {
                 <div class="card-body">
                  <h5 class="card-title">${more.phone_name}</h5>
                 <p class="card-text">${more.brand}</p>
-            <a id="load-button" onclick="phoneDetails('${more.slug}')" href="#" class="btn btn-primary">Phone Details</a>
+            <a onclick="phoneDetails('${more.slug}')" href="#" class="btn btn-primary">Phone Details</a>
         </div>
         </div>
     </div>
      `
      morePhone.appendChild(div)
 })
+toggleSpinner('none')
 }
 // end load more button 
 
@@ -84,6 +134,7 @@ const phoneDetails = detail => {
     fetch(url)
     .then(res => res.json())
     .then(data => displayDetails(data.data))
+    toggleSpinner('block')
 }
 
 // single phone display details
@@ -107,4 +158,5 @@ const displayDetails = phone => {
 </div>
 
     `
+    toggleSpinner('none')
 }
